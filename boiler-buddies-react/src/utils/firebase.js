ce002e1af2845befe038f6d2f9deaa47bbd01343
Meghna <mrajago@purdue.edu>
2023-02-24 17:47:42 -0500

    Merge main branch

--- boiler-buddies-react/src/utils/firebase.js
+++ boiler-buddies-react/src/utils/firebase.js
@@ -11,6 +11,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID,
+//   measurementId: process.env.REACT_APP_MEASUREMENTID
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyBpG9oWxS8VZGMRXDA7fieV_HLREAXqXnY",
   authDomain: "boilerbuddies-ec781.firebaseapp.com",
