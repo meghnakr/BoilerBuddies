c68601fe158cb3cddab8eb509e59b915f94746bb
meghnakr <54601079+meghnakr@users.noreply.github.com>
2023-02-24 18:37:52 -0500

    Update firebase.js

--- boiler-buddies-react/src/utils/firebase.js
+++ boiler-buddies-react/src/utils/firebase.js
@@ -11,24 +11,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APPID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTID
-// };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBpG9oWxS8VZGMRXDA7fieV_HLREAXqXnY",
-  authDomain: "boilerbuddies-ec781.firebaseapp.com",
-  projectId: "boilerbuddies-ec781",
-  storageBucket: "boilerbuddies-ec781.appspot.com",
-  messagingSenderId: "675053874671",
-  appId: "1:675053874671:web:f5bd3b6de02d10a4589a1a",
-  measurementId: "G-9L1D6C0VKZ"
+ const firebaseConfig = {
+   apiKey: process.env.REACT_APP_APIKEY,
+   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+   projectId: process.env.REACT_APP_PROJECTID,
+   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+   appId: process.env.REACT_APP_APPID,
+   measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 console.log("This is the project id: ", process.env.REACT_APP_PROJECTID)
