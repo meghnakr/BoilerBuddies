6a67bc3a5443e824f34a7c8a0dfbd57d1f573262
Samara Saquib <46725759+samarasaquib00@users.noreply.github.com>
2023-03-25 18:46:33 -0400

    Bug fix

--- boiler-buddies-react/src/App.js
+++ boiler-buddies-react/src/App.js
@@ -65,6 +65,7 @@ function App(location) {
               path="/notifications"
               element={<Notifications funcNav={setShowNav} />}
             />
+            {/*<Route path="/create-profile" element={<CreateProfile funcNav={setShowNav} />} />*/}
             <Route path="/create-forum" element={<CreateForum funcNav={setShowNav} />} />
           </Routes>
         </div>
