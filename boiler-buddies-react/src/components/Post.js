224b5c0342803d0f954fe030f8c8094d3fd70482
maevetra <ntra@purdue.edu>
2023-03-26 15:26:08 -0400

    Create components for post and comments

--- boiler-buddies-react/src/components/Post.js
+++ boiler-buddies-react/src/components/Post.js
@@ -4,94 +4,45 @@ import {endpoint} from '../global';
 
 export default  class Post extends React.Component {
     static propTypes = {
-        tokenId: PropTypes.string,
+        content: PropTypes.string
     }
 
     constructor(props) {
         super(props)
-        this.token = this.props.tokenId
+        this.username = this.props.username
+        this.postAt = this.props.postAt
+        this.content = this.props.content
+        this.img = this.props.img
         this.state = {
-            file: null,
-            file64: '',
-            content: ''
+            liked: this.props.liked,
+            likes: this.props.likes,
+            comments: this.props.comments
         }
     }
 
-    handleFileInputChange = (event) => {
-        var file = event.target.files[0]
-        this.setState({file: URL.createObjectURL(file)})
-        this.convertToBase64(file).then(result => {
-            this.setState({file64:result});
-        }).catch(err => {
-            console.log(err);
-          });
+    handleLike = () => {
+        if (this.state.liked) {
+            this.setState({liked: false, likes: this.state.likes--})
+        } else {
+            this.setState({liked: true, likes: this.state.likes++})
+        }
     }
-
-    handlePosting = () => {
-        if(this.state.content === '') {return;}
-        var params = new URLSearchParams();
-        params.append('token', this.token)
-        params.append('content', this.state.content)
-        params.append('bigImage', this.state.file64)
-        
-        var postRequestURL = endpoint + "addPost/?" + params
-        console.log(postRequestURL)
-        /*
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", postRequestURL, false ); // false for synchronous request
-        xmlHttp.send(null);
-        var postId = JSON.parse(xmlHttp.responseText).postId
-        console.log(postId);*/
-        
-    }
-
-    convertToBase64 = (file) => {
-        return new Promise(resolve => {
-            let baseURL = "";
-            // Make new FileReader
-            let reader = new FileReader();
-
-            // Convert the file to base64 text
-            reader.readAsDataURL(file);
-
-            // on reader load somthing...
-            reader.onload = () => {
-                // Make a fileInfo Object
-                baseURL = reader.result;
-                resolve(baseURL);
-            };
-        });
-    }
-    render() {
+    render () {
         const {
-            handlePosting,
-            handleFileInputChange,
-            state: {file, content}
+            content, img, username, postAt, 
+            state: {liked, likes, comments,}
         } = this
         return (
             <div className='post-container'>
-            <textarea rows='1' className='create-post' placeholder='Share your thoughts' type='text' value={content} onChange={(event)=>{this.setState({content:event.target.value})}}/>
-            {file && <div className='photo-uploaded-container'>
-                <button className='remove-img' onClick={() => {this.setState({file:null, file64:''})}}><i className='fa fa-times-circle'></i></button>
-                <img src={file} alt='img'/>
-            </div>}
-            <hr class="solid" ></hr>
-            <div className='post-utils-container' >
-                <label htmlFor="upload-photo"><i className='fa fa-file-image-o' style={{fontSize: '1.5em', fontWeight:'light', color: '#88BBF6'}}></i></label>
-                <input
-                            type="file"
-                            id="upload-photo"
-                            accept='image/*'
-                            style={{
-                                display: 'none'
-                            }}
-                            onChange={handleFileInputChange}
-                            />
-            </div>
-            <div className='post-button-container'>
-                <button className='default-btn' onClick={handlePosting}>Post</button>
-            </div>
-            
+                <p style={{color:"grey", fontSize:"smaller"}}>Post by <u> {username}</u> - {postAt} </p>
+                <p>{content}</p>
+                <div className='post-stats-container'>
+                    <button className='no-outline-btn'><i className='fa fa-comment-o'></i> {comments} Comments</button>
+                    <button className='no-outline-btn' onClick={() => this.setState(this.handleLike)} style={liked ? {color: 'red'} : {color:'grey'}}>
+                        <i className={liked ? 'fa fa-heart' : 'fa fa-heart-o'}></i> 
+                         {likes} Likes
+                    </button>
+                </div>
             </div>
         )
     }
--- boiler-buddies-react/src/pages/feed.js
+++ boiler-buddies-react/src/pages/feed.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import Post from '../components/Post';
+import NewPost from '../components/NewPost';
 import useUser from '../hooks/useUser';
+import { useState } from 'react';
+import Post from '../components/Post';
 
 const Feed = (props) => {
-    const currentuser = useUser()
-
     props.funcNav(true);
+    const currentuser = useUser()
+    const [postId , setPostId] = useState([]);
+
+    function getPostId (newPost) {
+        setPostId(postId => [...postId, newPost])
+    }
+
+
+    const dummyPostId = [
+        {content: "Content of test post 1", username:"JaneDoe",  postAt:"March 26, 2023", likes:"20", comments:"2", liked:true, img:null},
+        {content: "Content of test post 2", username:"Blahblah",  postAt:"March 22, 2023", likes:"10", comments:"5", liked:false, img:null},
+    ]
     return ( 
         <div className='page-container'>
-            <Post tokenId={currentuser.token}/>
-            
+            <NewPost tokenId={currentuser.token} handleCallback={getPostId}/>
+            <div className='all-post'>
+                {dummyPostId.map(post => {
+                    return <Post content={post.content} username={post.username} postAt={post.postAt} likes={post.likes} comments={post.comments} liked={post.liked}/>
+                })}
+                
+            </div>
         </div>
     )
 };
