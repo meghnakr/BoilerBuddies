ad02ca2bf448dfd97cf92ec19bdc1f3c0ea66675
maevetra <ntra@purdue.edu>
2023-03-27 18:11:56 -0400

    add validation for new post

--- boiler-buddies-react/src/pages/feed.js
+++ boiler-buddies-react/src/pages/feed.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NewPost from '../components/NewPost';
 import useUser from '../hooks/useUser';
 import { useState } from 'react';
 import Post from '../components/Post';
+import { endpoint } from '../global';
 
 const Feed = (props) => {
     props.funcNav(true);
     const currentuser = useUser()
     const [postId , setPostId] = useState([]);
+    const [forums, setForums] = useState([]);
+
+    useEffect(() => {
+        var getForumsRequestURL = endpoint + "getForums/?"
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", getForumsRequestURL, false); // false for synchronous request
+        xmlHttp.send(null);
+        var response = JSON.parse(xmlHttp.responseText);
+        var formatForum = []
+        response.map(element => {
+            formatForum.push({value: element.id, label: element.name})
+        });
+        console.log(formatForum)
+        setForums(formatForum)
+    }, [])
 
     function getPostId (newPost) { //this function is called every time the "Post" button is clicked
         setPostId(postId => [...postId, newPost]) //add new post to array
     }
 
+    
 
     const dummyPostId = [
         {content: "Content of test post 1", username:"JaneDoe",  postAt:"March 26, 2023", likes:"20", comments:"2", liked:true, img:null},
@@ -21,7 +38,7 @@ const Feed = (props) => {
     ]
     return ( 
         <div className='page-container'>
-            <NewPost tokenId={currentuser.token} handleCallback={getPostId}/>
+            <NewPost tokenId={currentuser.token} handleCallback={getPostId} forums={forums}/>
             <div className='all-post'>
                 {dummyPostId.map(post => {
                     return <Post content={post.content} username={post.username} postAt={post.postAt} likes={post.likes} comments={post.comments} liked={post.liked}/>
