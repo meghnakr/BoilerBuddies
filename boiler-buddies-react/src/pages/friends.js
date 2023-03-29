b04a7da3f0043c778f7361c4b6cf9d1d7ab5c6b1
Samara Saquib <46725759+samarasaquib00@users.noreply.github.com>
2023-03-28 22:06:31 -0400

    Friends List in Friends Tab

--- boiler-buddies-react/src/pages/friends.js
+++ boiler-buddies-react/src/pages/friends.js
@@ -21,6 +21,22 @@ const Friends = () => {
   //   });
   // }, []); // dependency array
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const token = await getusertoken();
+  //     const result = await axios.get("/getFriends/", {
+  //       params: {
+  //         //token: "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OWVkMTU1OTdhYjM1Zjc4MjljZTc0NDMwN2I3OTNiN2ViZWIyZjAiLCJ0eXAiOiJKV1QifQ.eyJ1c2VySWQiOiIwOWFhMTE3Ny03ZTNkLTRjODMtOTQ0Ni05MDc5YzA0OGY1ZmUiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYm9pbGVyYnVkZGllcy1lYzc4MSIsImF1ZCI6ImJvaWxlcmJ1ZGRpZXMtZWM3ODEiLCJhdXRoX3RpbWUiOjE2Nzk5NjYxNjAsInVzZXJfaWQiOiJ1UFJ1dEM1ZGdwYm5GTms3WEtzWDJDYzg1VXIyIiwic3ViIjoidVBSdXRDNWRncGJuRk5rN1hLc1gyQ2M4NVVyMiIsImlhdCI6MTY3OTk4MDkyNCwiZXhwIjoxNjc5OTg0NTI0LCJlbWFpbCI6ImFzYXF1aWJAcHVyZHVlLmVkdSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFzYXF1aWJAcHVyZHVlLmVkdSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.Ao5gPJDGVrds1ADi82a3_t-1N0l8szUcqoMj1s3fTzw1Z3EIeJtSI7t3lR07VC11oXTyn9UIclqS22sCexxxHdTizfk4IILVco8W1tBF4Xce7pdDxcMpimfbvBX2k4-qHjvGdrnZMy1Df-mpGFuGlMm1jJ3voxEKgUAW-8-mhLjTe31go5yE4XHvcFVHBBd3GK62-LcE2Hw0CMu_cralH6L3C63lic2ItV1wXSzpPFR3DCU6lfaP7lz-eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OWVkMTU1OTdhYjM1Zjc4MjljZTc0NDMwN2I3OTNiN2ViZWIyZjAiLCJ0eXAiOiJKV1QifQ.eyJ1c2VySWQiOiIyNzI1OWNlMy0zZjc5LTQzNzMtOGRhYi1lZGFmZGEyMTczZjgiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYm9pbGVyYnVkZGllcy1lYzc4MSIsImF1ZCI6ImJvaWxlcmJ1ZGRpZXMtZWM3ODEiLCJhdXRoX3RpbWUiOjE2Nzk5NjE3MDYsInVzZXJfaWQiOiJkaU9iaEFVdklOZnhsUXlWMzNMZ1Z4dW1vWDYyIiwic3ViIjoiZGlPYmhBVXZJTmZ4bFF5VjMzTGdWeHVtb1g2MiIsImlhdCI6MTY3OTk4MTM0NywiZXhwIjoxNjc5OTg0OTQ3LCJlbWFpbCI6ImhwYm9pbGVyYnVkZHlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiaHBib2lsZXJidWRkeUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.XDbtv2M3RHkRr76VCtGSmqwgCnVjF9LxEkgPmg4wIQcE_PTtDOe26Po_unhQwXENYpnszvg9R3LFF-xh3H2HEnDYA_WjgEmNMtykSjH1apm9mhKqimEwlC8uo1eslKaVgCYeiyklRKSBGitgO5DcixDGlLdlrrYoWGCoG-HFC3f0bOSpiq4NxOkUynMMB4AHzSCeuz_5YbPzHuSTohZH4czCFWvYH1LGDOjKknNMzV7G3SNlmOMfijM_1XbXLtZ7JWQ-UViQ3D9hTBdWkHu0Yow8qTAQHK0l_ofRZWa3Wrn-vmMeCQihjevrmum377LjRHjMBCNj6-zsP8yzBjTCyg",
+  //         token: token,
+  //       },
+  //     });
+  //     console.log("TOKEN A: ", getusertoken());
+  //     console.log("Result data:", result.data);
+  //     setFriendProfs(Object.values(result.data));
+  //   };
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       const token = await getusertoken();
@@ -30,54 +46,54 @@ const Friends = () => {
           token: token,
         },
       });
-      console.log("TOKEN A: ", getusertoken());
-      console.log("Result data:", result.data);
-      setFriendProfs(Object.values(result.data));
+      //console.log("TOKEN A: ", getusertoken());
+      //console.log("Result data:", result.data);
+      /* Call getUserById */
+      const mappedUsers = await Promise.all(
+        Object.values(result.data).map(
+          // how to solve array of Promises
+          async (currentfriend, index) => {
+            const otherId = currentfriend.other_id;
+            //console.log("OtherId:", otherId);
+            const userObject = await axios.get("/getUserById/", {
+              // must be in an async function
+              params: {
+                user_id: otherId,
+              },
+            });
+            //          console.log(userObject.data);
+            return await userObject.data;
+          }
+        )
+      ); // closes map
+      console.log("Mapped users: ", mappedUsers);
+      setFriendProfs(mappedUsers);
     };
     fetchData();
   }, []);
 
-  console.log("FRIEND PROFS: ", friendProfs)
+  console.log("FRIEND PROFS: ", friendProfs);
 
   return (
     <div className="page-container">
       <form className="notifs-content">
         <span style={{ fontWeight: "bold", fontSize: 20 }}>Friends: </span>
-
-        {/* {friendProfs.map(
-          (
-            currentfriendrequest,
-            index // goes through each element in friendReqs and maps it out by index
-          ) => (
-            <FriendsTabProfile
-              username={currentfriendrequest.username}
-              displayName={currentfriendrequest.displayName}
-              interestTags={currentfriendrequest.interestTags}
-              userId={currentfriendrequest.userId} //user_id property
-              key={index}
-            />
-          )
-        )} */}
-
         {friendProfs.length > 0 ? (
-          friendProfs.map((currentfriendrequest, index) => (
-            <FriendsTabProfile
-              username={currentfriendrequest.username}
-              displayName={currentfriendrequest.displayName}
-              interestTags={currentfriendrequest.interestTags}
-              userId={currentfriendrequest.userId}
-              key={index}
-            />
-          ))
+          friendProfs.map((currentfriend, index) => {
+            console.log("Current Friend Request: ", currentfriend);
+            return (
+              <FriendsTabProfile
+                username={currentfriend.username}
+                displayName={currentfriend.display_name}
+                interestTags={currentfriend.interests}
+                userId={currentfriend.user_id}
+                key={index}
+              />
+            );
+          })
         ) : (
           <p>No friends</p>
         )}
-
-        {/*Array(5)
-          .fill(0)
-          .map(() => (
-            <FriendsTabProfile />
-          ))*/}
       </form>
     </div>
   );
