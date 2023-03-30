eda2cfbaf577d0989e85eb8cd1e7eb5775f33e01
Samara Saquib <46725759+samarasaquib00@users.noreply.github.com>
2023-03-30 00:00:29 -0400

    Delete example box

--- boiler-buddies-react/src/components/ConfirmationBox.js
+++ boiler-buddies-react/src/components/ConfirmationBox.js
@@ -20,25 +20,3 @@ export default class ConfirmationBox extends React.Component {
   }
 }
 
-function App() {
-  const handleYes = () => {
-    console.log("Yes");
-  };
-
-  const handleNo = () => {
-    console.log("No");
-  };
-
-  return (
-    <div>
-      <h1>Confirmation Box Example</h1>
-      <ConfirmationBox
-        message="Are you sure you want to delete this item?"
-        onYes={handleYes}
-        onNo={handleNo}
-      />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
